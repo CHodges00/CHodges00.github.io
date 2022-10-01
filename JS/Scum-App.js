b95ba7a21c80914e8d983ff31d92de6b3219f7b2
Cody Hodges <hodges.cody00@gmail.com>
2022-10-01 14:38:49 -0500

    Finished Weapon functionality

--- JS/Scum-App.js
+++ JS/Scum-App.js
@@ -14,18 +14,18 @@ let walls = function () {
 
 
 let weapons = function () {
-    if (window.localStorage.getItem("walls") === null) {
+    if (window.localStorage.getItem("weapons") === null) {
         return [
-            {id: 1, name: 'Improvised Mine', image: "./IMG/imp-mine.jpg"},
-            {id: 2, name: 'Improvised Claymore', image: "./IMG/imp-claymore.jpg"},
-            {id: 3, name: 'Claymore', image: "./IMG/claymore.jpg"},
-            {id: 4, name: 'Anti-personnel Mine', image: "./IMG/anti.jpg"},
-            {id: 5, name: 'Small Anti-personnel Mine', image: "./IMG/small-anti-personnel.jpg"},
-            {id: 6, name: 'Prom-1', image: "./IMG/prom.jpg"},
-            {id: 7, name: 'M82', image: "./IMG/m82.jpg"},
+            {id: 1, name: 'Improvised Mine', image: "./IMG/imp-mine.jpg", damage: 0},
+            {id: 2, name: 'Improvised Claymore', image: "./IMG/imp-claymore.jpg", damage: 0},
+            {id: 3, name: 'Claymore', image: "./IMG/claymore.jpg", damage: 0},
+            {id: 4, name: 'Anti-personnel Mine', image: "./IMG/anti.jpg", damage: 0},
+            {id: 5, name: 'Small Anti-personnel Mine', image: "./IMG/small-anti-personnel.jpg", damage: 0},
+            {id: 6, name: 'Prom-1', image: "./IMG/prom.jpg", damage: 0},
+            {id: 7, name: 'M82', image: "./IMG/m82.jpg", damage: 0},
         ]
     } else {
-        return JSON.parse(window.localStorage.getItem("walls"));
+        return JSON.parse(window.localStorage.getItem("weapons"));
     }
 }();
 
@@ -59,7 +59,7 @@ function createWeaponColumns(weapons) {
 function createWeaponHTML(weapon) {
 
     let html = '<div class= "col-md-4 col-lg-4 weapons" style="text-align: center"><div><img style="width: 200px; height: 200px" src="';
-    html += weapon.image + '"></div><div><h2>' + weapon.name.toUpperCase() + '</h2></div><button type="button" id="'+ weapon.id +'" onclick="amountNeeded()" class="btn btn-md btn-primary">SELECT</button><hr></div>';
+    html += weapon.image + '"></div><div><h2>' + weapon.name.toUpperCase() + '</h2></div><button type="button" id="'+ weapon.id +'" onclick="amountNeeded('+ weapon.id +')" class="btn btn-md btn-primary">SELECT</button><hr></div>';
 
     return html;
 };
@@ -142,22 +142,351 @@ function wallLife(id){
     }
 
 
-    let html = '<div class="col bg-dark"><h3 hidden id="wllLength">'+ wallLength.options[wallLength.selectedIndex].value +'</h3><h3 hidden id="wllId">'+ id +'</h3><h3 hidden id="number">'+ numberOfWalls.options[numberOfWalls.selectedIndex].value +'</h3><h1 style="color: red">Type = '+ walls[wall.id - 1].name.toUpperCase() +'</h1><h1 style="color: red">Length = '+ wallLength.options[wallLength.selectedIndex].value +'</h1><h1 style="color: red">Number = '+ numberOfWalls.options[numberOfWalls.selectedIndex].value +'</h1><h1 style="color: red">Total Health = <span id="ttlHlth">'+ wallTotalHealth(lengthValue, numberValue)+'</span></h1></div>'
+    let html = '<div class="col bg-dark text-danger"><h3 hidden id="wllLength">'+ wallLength.options[wallLength.selectedIndex].value +'</h3><h3 hidden id="wllId">'+ id +'</h3><h3 hidden id="number">'+ numberOfWalls.options[numberOfWalls.selectedIndex].value +'</h3><h1>Type = '+ walls[wall.id - 1].name.toUpperCase() +'</h1><h1>Length = '+ wallLength.options[wallLength.selectedIndex].value +'</h1><h1>Number = '+ numberOfWalls.options[numberOfWalls.selectedIndex].value +'</h1><h1>Total Health = <span id="ttlHlth">'+ wallTotalHealth(lengthValue, numberValue)+'</span></h1></div>'
 
     let wallHealthArea = document.querySelector('.total');
     wallHealthArea.innerHTML = html;
 }
 
 
-function amountNeeded(){
+function amountNeeded(id){
+    var weapon = weapons[id - 1];
+    var damage = weapon.damage;
     var wallHealth = document.querySelector('#ttlHlth').innerHTML;
     var wallLength = document.querySelector('#wllLength').innerHTML;
     var wallId =  document.querySelector('#wllId').innerHTML;
     var numberOfWalls = document.querySelector('#number').innerHTML
+
+
+    //==============================================================================================
+
+    //IMP MINE START
+    //imp mine on wood
+    if (id === 1 && parseInt(wallId) === 1 && parseInt(wallLength) === 1){
+        damage = 11
+    }else if (id === 1 && parseInt(wallId) === 1 && parseInt(wallLength) === 2){
+        damage = 11
+    }
+    else if (id === 1 && parseInt(wallId) === 1 && parseInt(wallLength) === 3){
+        damage = 11
+    }
+    else if (id === 1 && parseInt(wallId) === 1 && parseInt(wallLength) === 4){
+        damage = 11
+    }
+    else if (id === 1 && parseInt(wallId) === 1 && parseInt(wallLength) === 5){
+        damage = 12
+    }
+    // imp mine on wire
+    else if (id === 1 && parseInt(wallId) === 2 && parseInt(wallLength) === 1){
+        damage = 9
+    }
+    else if (id === 1 && parseInt(wallId) === 2 && parseInt(wallLength) === 2){
+        damage = 9
+    }
+    else if (id === 1 && parseInt(wallId) === 2 && parseInt(wallLength) === 3){
+        damage = 10
+    }
+    else if (id === 1 && parseInt(wallId) === 2 && parseInt(wallLength) === 4){
+        damage = 9
+    }
+    else if (id === 1 && parseInt(wallId) === 2 && parseInt(wallLength) === 5){
+        damage = 9
+    }
+    //imp mine on metal
+    else if (id === 1 && parseInt(wallId) === 3 && parseInt(wallLength) === 1){
+        damage = 8
+    }
+    else if (id === 1 && parseInt(wallId) === 3 && parseInt(wallLength) === 2){
+        damage = 7
+    }
+    else if (id === 1 && parseInt(wallId) === 3 && parseInt(wallLength) === 3){
+        damage = 8
+    }
+    else if (id === 1 && parseInt(wallId) === 3 && parseInt(wallLength) === 4){
+        damage = 11
+    }
+    else if (id === 1 && parseInt(wallId) === 3 && parseInt(wallLength) === 5){
+        damage = 7
+    }
+    //imp mine on brick
+    else if (id === 1 && parseInt(wallId) === 4 && parseInt(wallLength) === 1){
+        damage = 4
+    }
+    else if (id === 1 && parseInt(wallId) === 4 && parseInt(wallLength) === 2){
+        damage = 4
+    }
+    else if (id === 1 && parseInt(wallId) === 4 && parseInt(wallLength) === 3){
+        damage = 4
+    }
+    else if (id === 1 && parseInt(wallId) === 4 && parseInt(wallLength) === 4){
+        damage = 4
+    }
+    else if (id === 1 && parseInt(wallId) === 4 && parseInt(wallLength) === 5){
+        damage = 4
+    }
+    //imp mine on concrete
+    else if (id === 1 && parseInt(wallId) === 5 && parseInt(wallLength) === 1){
+        damage = 3
+    }
+    else if (id === 1 && parseInt(wallId) === 5 && parseInt(wallLength) === 2){
+        damage = 3
+    }
+    else if (id === 1 && parseInt(wallId) === 5 && parseInt(wallLength) === 3){
+        damage = 3
+    }
+    else if (id === 1 && parseInt(wallId) === 5 && parseInt(wallLength) === 4){
+        damage = 3
+    }
+    else if (id === 1 && parseInt(wallId) === 5 && parseInt(wallLength) === 5){
+        damage = 3
+    }
+    //IMP MINE END
+
+        //==============================================================================================
+
+    //IMP CLAYMORE START
+
+    //imp clay on wood
+    if(id === 2 && parseInt(wallId) === 1 && parseInt(wallLength) < 6){
+        damage = 31
+    }
+    //imp clay on wire
+    else if(id === 2 && parseInt(wallId) === 2 && parseInt(wallLength) < 6){
+        damage = 26
+    }
+    //imp clay on metal
+    else if(id === 2 && parseInt(wallId) === 3 && parseInt(wallLength) === 1){
+        damage = 21
+    }
+    else if(id === 2 && parseInt(wallId) === 3 && parseInt(wallLength) === 2){
+        damage = 21
+    }
+    else if(id === 2 && parseInt(wallId) === 3 && parseInt(wallLength) === 3){
+        damage = 18
+    }
+    else if(id === 2 && parseInt(wallId) === 3 && parseInt(wallLength) === 4){
+        damage = 17
+    }
+    else if(id === 2 && parseInt(wallId) === 3 && parseInt(wallLength) === 5){
+        damage = 21
+    }
+    //imp clay on brick
+    else if(id === 2 && parseInt(wallId) === 4 && parseInt(wallLength) < 6){
+        damage = 10
+    }
+    // imp clay on concrete
+    else if(id === 2 && parseInt(wallId) === 5 && parseInt(wallLength) < 6) {
+        damage = 8
+    }
+
+
+    //IMP CLAYMORE END
+
+        //==============================================================================================
+
+    //CLAYMORE START
+
+    //clay on wood
+    if(id === 3 && parseInt(wallId) === 1 && parseInt(wallLength) < 3){
+        damage = 52
+    }
+    else if(id === 3 && parseInt(wallId) === 1 && parseInt(wallLength) === 3) {
+        damage = 51
+    }
+    else if(id === 3 && parseInt(wallId) === 1 && parseInt(wallLength) === 4) {
+        damage = 52
+    }
+    else if(id === 3 && parseInt(wallId) === 1 && parseInt(wallLength) === 5) {
+        damage = 51
+    }
+    //clay on wire
+    else if(id === 3 && parseInt(wallId) === 2 && parseInt(wallLength) === 1) {
+        damage = 46
+    }
+    else if(id === 3 && parseInt(wallId) === 2 && parseInt(wallLength) === 2) {
+        damage = 44
+    }
+    else if(id === 3 && parseInt(wallId) === 2 && parseInt(wallLength) === 3) {
+        damage = 45
+    }
+    else if(id === 3 && parseInt(wallId) === 2 && parseInt(wallLength) === 4) {
+        damage = 47
+    }
+    else if(id === 3 && parseInt(wallId) === 2 && parseInt(wallLength) === 5) {
+        damage = 41
+    }
+    //clay on metal
+    else if(id === 3 && parseInt(wallId) === 3 && parseInt(wallLength) === 1) {
+        damage = 36
+    }
+    else if(id === 3 && parseInt(wallId) === 3 && parseInt(wallLength) === 2) {
+        damage = 35
+    }
+    else if(id === 3 && parseInt(wallId) === 3 && parseInt(wallLength) === 3) {
+        damage = 35
+    }
+    else if(id === 3 && parseInt(wallId) === 3 && parseInt(wallLength) === 4) {
+        damage = 36
+    }
+    else if(id === 3 && parseInt(wallId) === 3 && parseInt(wallLength) === 5) {
+        damage = 34
+    }
+    //clay on brick
+    else if(id === 3 && parseInt(wallId) === 4 && parseInt(wallLength) === 1) {
+        damage = 19
+    }
+    else if(id === 3 && parseInt(wallId) === 4 && parseInt(wallLength) === 2) {
+        damage = 18
+    }
+    else if(id === 3 && parseInt(wallId) === 4 && parseInt(wallLength) === 3) {
+        damage = 18
+    }
+    else if(id === 3 && parseInt(wallId) === 4 && parseInt(wallLength) === 4) {
+        damage = 20
+    }
+    else if(id === 3 && parseInt(wallId) === 4 && parseInt(wallLength) === 5) {
+        damage = 18
+    }
+    //clay on concrete
+    else if(id === 3 && parseInt(wallId) === 5 && parseInt(wallLength) < 5) {
+        damage = 15
+    }
+    else if(id === 3 && parseInt(wallId) === 5 && parseInt(wallLength) === 5) {
+        damage = 14
+    }
+
+
+    //CLAYMORE END
+
+        //==============================================================================================
+
+    //SMALL AP MINE START
+
+    //small ap on wood
+    if(id === 4 && parseInt(wallId) === 1 && parseInt(wallLength) < 6){
+        damage = 11
+    }
+    //small ap on wire
+    else if(id === 4 && parseInt(wallId) === 2 && parseInt(wallLength) < 6) {
+        damage = 9
+    }
+    //small ap on metal
+    else if(id === 4 && parseInt(wallId) === 3 && parseInt(wallLength) < 6) {
+        damage = 7
+    }
+    //small ap on brick
+    else if(id === 4 && parseInt(wallId) === 4 && parseInt(wallLength) < 6) {
+        damage = 3
+    }
+    //small ap on concrete
+    else if(id === 4 && parseInt(wallId) === 5 && parseInt(wallLength) < 6) {
+        damage = 3
+    }
+
+
+    //SMALL AP MINE END
+
+    //==============================================================================================
+
+    //AP MINE START
+    //ap on wood
+    if(id === 5 && parseInt(wallId) === 1 && parseInt(wallLength) < 6){
+        damage = 11
+    }
+    //ap on wire
+    else if(id === 5 && parseInt(wallId) === 2 && parseInt(wallLength) < 6) {
+        damage = 9
+    }
+    //ap on metal
+    else if(id === 5 && parseInt(wallId) === 3 && parseInt(wallLength) < 6) {
+        damage = 7
+    }
+    //ap on brick
+    else if(id === 5 && parseInt(wallId) === 4 && parseInt(wallLength) < 6) {
+        damage = 3
+    }
+    //ap on concrete
+    else if(id === 5 && parseInt(wallId) === 5 && parseInt(wallLength) < 6) {
+        damage = 3
+    }
+
+    //AP MINE END
+
+    //==============================================================================================
+
+    //PROM 1 START
+
+    //prom 1 on wood
+    if(id === 6 && parseInt(wallId) === 1 && parseInt(wallLength) < 6){
+        damage = 11
+    }
+    //prom 1 on wire
+    else if(id === 6 && parseInt(wallId) === 2 && parseInt(wallLength) < 6) {
+        damage = 9
+    }
+    //prom 1 on metal
+    else if(id === 6 && parseInt(wallId) === 3 && parseInt(wallLength) < 6) {
+        damage = 7
+    }
+    //prom 1 on brick
+    else if(id === 6 && parseInt(wallId) === 4 && parseInt(wallLength) < 6) {
+        damage = 3
+    }
+    //prom 1 on concrete
+    else if(id === 6 && parseInt(wallId) === 5 && parseInt(wallLength) < 6) {
+        damage = 3
+    }
+
+    //PROM 1 END
+
+    //==============================================================================================
+
+    //M82 START
+
+    //M82 on wood
+    if(id === 7 && parseInt(wallId) === 1 && parseInt(wallLength) < 6){
+        damage = 16
+    }
+    //M82 on wire
+    else if(id === 7 && parseInt(wallId) === 2 && parseInt(wallLength) < 6) {
+        damage = 16
+    }
+    //M82 on metal
+    else if(id === 7 && parseInt(wallId) === 3 && parseInt(wallLength) < 6) {
+        damage = 16
+    }
+    //M82 on brick
+    else if(id === 7 && parseInt(wallId) === 4 && parseInt(wallLength) < 6) {
+        damage = 11
+    }
+    //M82 on concrete
+    else if(id === 7 && parseInt(wallId) === 5 && parseInt(wallLength) < 6) {
+        damage = 9
+    }
+
+    //M82 END
+
+    //==============================================================================================
+
+    function weaponsNeeded(wallHealth, damage){
+        return wallHealth / damage;
+    }
+
+    let Finalhtml = '<div class="col bg-dark text-danger"><h1>You need<br> '+ weaponsNeeded(wallHealth, damage).toFixed(2) +' '+ weapon.name +'<br> to destroy '+ numberOfWalls +'<br> '+ wallLength +'m '+ walls[wallId - 1].name.toUpperCase() +' walls</h1></div>'
+
+    let finalArea = document.querySelector('.final');
+    finalArea.innerHTML = Finalhtml;
+
+
+
     console.log('# of walls: '+ numberOfWalls);
     console.log('wall id: ' + wallId);
     console.log('length: ' + wallLength);
     console.log('health: ' + wallHealth);
+    console.log('weapon id: ' + id)
+    console.log('weapon name: ' + weapon.name);
+    console.log('weapon damage: ' + damage)
+
 }
 
 
